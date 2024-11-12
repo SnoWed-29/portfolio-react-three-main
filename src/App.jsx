69f9618d3dd7e49c00107c6dad1d8e680a50b8d7
Snowed29 <snowe29@gmail.com>
2024-11-12 19:52:52 +0100

    fixing side bar rendring

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import "./App.css";
 import AnimatedBackground from "./common/AnimatedBackground";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import Content from "./components/Content";
 import Sidebar from "./components/Sidebar";
-import LineAnimation from "./common/LineAnimation";
 import Expriences from "./pages/Expriences";
 
 function App() {
@@ -13,13 +11,15 @@ function App() {
     <Router>
       <AnimatedBackground>
         <div className="w-[90vw] md:h-screen p-4 flex flex-col items-start md:items-center justify-center mx-auto">
-          <div className="grid grid-cols-3  ">
+          <div className="grid grid-cols-3">
             <Sidebar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/experiences" element={<Expriences />} />
-            </Routes>
+            <div className="col-span-2">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/experiences" element={<Expriences />} />
+              </Routes>
+            </div>
           </div>
         </div>
       </AnimatedBackground>
